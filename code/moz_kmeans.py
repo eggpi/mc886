48d4f2383e34bf45a703b8474a78f06c1a47940b
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2013-09-03 15:15:16 -0300

    More kmeans hacking.

--- code/moz_kmeans.py
+++ code/moz_kmeans.py
@@ -1,3 +1,4 @@
+import time
 import pprint
 import sqlite3
 from urllib2 import urlparse
@@ -5,8 +6,30 @@ from collections import defaultdict
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plot
+
+"""
+A sketch for prediction based on k-means.
+
+The basic idea is to use k-means to cluster subresources fetched by the
+pages under a domain based on their number of hits and timestamp of the last
+hit. These clusters should roughly correspond to the pages, or groups of pages
+accessed close together.
+
+Whenever we want to predict a new page load, we look for the cluster to which
+that page fits more closely (in the future the page should actually belong to a
+cluster), and take predictive actions for everything there.
+
+Unanswered questions:
+    - How to pick a suitable K?
+    - We need to normalize the data for k-means to work well. How can we
+      normalize a timestamp?
+    - Are we going to run k-means on every prediction request (that would make
+      normalization easy)? How expensive is that? Look into online kmeans!
+"""
 
 K = 3 # FIXME needs tweaking
+now = 1378215391951426.0 # very rudimentary normalization
 
 def load_db(dbfile):
     db = sqlite3.connect(dbfile)
@@ -68,10 +91,10 @@ def cluster_subresources_for_hosts(hosts, pages, subresources):
             # make a numpy array out of the hit count and
             # timestamp for each subresource
             sres_vectors = np.array(
-                [sres[-2:] for sres in host_sres],
+                [(sres[-2], sres[-1] / now) for sres in host_sres],
                 dtype = 'float32')
 
-            _, clusters, means = cv2.kmeans(
+            retval, clusters, means = cv2.kmeans(
                 sres_vectors,
                 K = K,
                 criteria = (cv2.TERM_CRITERIA_MAX_ITER, 10, 0), # 10 iterations
@@ -79,8 +102,8 @@ def cluster_subresources_for_hosts(hosts, pages, subresources):
                 flags = cv2.KMEANS_RANDOM_CENTERS)
 
             # transform clusters into list of tuples:
-            # [ (mean, list of subresources for cluster 0),
-            #   (mean, list of subresources for cluster 1), ...
+            # [ (mean, [list of subresources for cluster 0]),
+            #   (mean, [list of subresources for cluster 1]), ...
             # ]
             clusters_for_hosts[host] = []
             for mean in means:
@@ -108,7 +131,9 @@ def predict_for_page_load(uri, pages, clusters_for_hosts, hosts):
     page = find_page_by_uri(uri, pages)
 
     means = [c[0] for c in clusters]
-    distances = map(lambda mean: np.linalg.norm(mean - page[-2:]), means)
+    distances = map(
+        lambda mean: np.linalg.norm(mean - (page[-2], page[-1] / now)),
+        means)
 
     # find the closest cluster
     closest = None
@@ -122,12 +147,30 @@ def predict_for_page_load(uri, pages, clusters_for_hosts, hosts):
     print 'Would take predictive actions for:'
     pprint.pprint([sr[2] for sr in sres])
 
+    visualize(page, clusters, closest)
+
+def visualize(page, clusters, closest_cluster):
+    colors = [
+        'black',
+        'yellow',
+        'green'
+    ]
+
+    assert len(colors) >= K - 1
+
+    plot.plot(page[-2], page[-1] / now, color = 'brown', marker = '^')
+    for cidx, (mean, sres) in enumerate(clusters):
+        color = 'red' if cidx == closest_cluster else colors.pop(0)
+
+        for sr in sres:
+            plot.plot(sr[-2], sr[-1] / now, color = color, marker = 'o')
+
+    plot.title("hits x normalized timestamp")
+    plot.show()
+
 if __name__ == "__main__":
     import sys
 
     hosts, pages, subresources = load_db(sys.argv[1])
     clusters_for_hosts = cluster_subresources_for_hosts(hosts, pages, subresources)
-
-    predict_for_page_load(
-        "http://docs.scipy.org/doc/scipy/reference/cluster.vq.html",
-        pages, clusters_for_hosts, hosts)
+    predict_for_page_load(sys.argv[2], pages, clusters_for_hosts, hosts)
