58814a2af23614574213d6849ae9bfa29170b2ab
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2013-09-17 15:41:16 -0300

    Fix histograms to that subresources are grouped together

--- code/histograms.py
+++ code/histograms.py
@@ -9,21 +9,21 @@ cursor.execute('SELECT COUNT(*) FROM moz_subresources GROUP BY pid;')
 counts = cursor.fetchall()
 
 plot.subplot(311)
-plot.hist([c[0] for c in counts], bins = 10)
+plot.hist([c[0] for c in counts], bins = 100, range = (0, 50))
 plot.title('Subresources per page')
 
-cursor.execute('SELECT last_hit FROM moz_subresources;')
+cursor.execute('SELECT max(last_hit) FROM moz_subresources group by uri;')
 last_loads = cursor.fetchall()
 
 plot.subplot(312)
-plot.hist([l[0] for l in last_loads], bins = 10)
+plot.hist([l[0] for l in last_loads], bins = 100)
 plot.title('Timestamp per subresource')
 
-cursor.execute('SELECT hits FROM moz_subresources;')
+cursor.execute('SELECT sum(hits) FROM moz_subresources group by uri;')
 hits = cursor.fetchall()
 
 plot.subplot(313)
-plot.hist([h[0] for h in hits], bins = 10)
+plot.hist([h[0] for h in hits], bins = 100)
 plot.title('Hits per subresource')
 
 plot.show()
